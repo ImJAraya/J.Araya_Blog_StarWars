e2afb628c3d8a7a85804487124f29ddd9bdce60e
ImJAraya <fabiarayamontero@gmail.com>
2025-04-11 09:23:23 -0600

    agreg perfil de gitHub

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			J.Araya
+			<a href="https://github.com/ImJAraya">J.Araya</a>
 		</p>
 	</footer>
 );
